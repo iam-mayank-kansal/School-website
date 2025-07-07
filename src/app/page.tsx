1058e5376507e29c753dd9e2d6271c4c7ceef30b
Amarjeet261 <amarjeetrajput326@gmail.com>
2025-07-07 14:11:19 +0530

    work in progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import ApplyNow from "@/components/applynow";
 import ContactUs from "@/components/contactus";
 import Event from "@/components/event";
 import Footer from "@/components/Footer";
+import SchoolGallery from "@/components/gallery";
 import Header from "@/components/header";
 import Navbar from "@/components/Navbar";
+import { SchoolInfo } from "@/components/school-Info";
 import Subscribe from "@/components/Subscribe";
 
 export default function Home() {
@@ -14,6 +16,8 @@ export default function Home() {
       <Event />
       <ApplyNow />
       <Subscribe />
+      <SchoolGallery/>
+      <SchoolInfo/>
       <ContactUs />
       <Footer />
     </>
